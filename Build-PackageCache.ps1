2f881c005dabae93224a4163e9f000c11f8b31b7
Jaime Still <jpstill85@gmail.com>
2022-07-08 23:11:02 -0400

    Update Build-PackageCache.ps1

--- Build-PackageCache.ps1
+++ Build-PackageCache.ps1
@@ -13,6 +13,8 @@ param(
 	$SkipClean
 )
 
+. .\functions.ps1
+
 $data = Get-Content -Raw -Path $Source | ConvertFrom-Json
 
 $sln = "src/BuildCache"
@@ -20,50 +22,7 @@ if (Test-Path $sln) {
 	Remove-Item $sln -Recurse -Force
 }
 
-#region Build-Solution
-
-function Get-ProjectPath(
-	[string] $sln,
-	[string] $project
-) {
-	return Join-Path $sln $project
-}
-
-function Build-Project([psobject] $project, [string] $sln) {
-	$output = Get-ProjectPath $sln $project.name
-
-	& dotnet new $project.template -o $output
-	& dotnet sln $sln add $output
-
-	$project.dependencies | ForEach-Object {
-		& dotnet add $output package $_
-	}
-}
-
-function Build-Solution([psobject] $data, [string] $sln) {
-	& dotnet new sln -o $sln
-
-	$data | ForEach-Object {
-		Build-Project $_ $sln
-	}
-}
-
 Build-Solution $data $sln
-
-#endregion
-
-#region Build-Cache
-
-function Initialize-Path([string] $path, [string] $type) {
-	if (-not (Test-Path $path)) {
-		New-Item -Path $path -ItemType $type -Force
-	}
-}
-
-function Build-Cache([string] $cache, [string] $sln) {
-	Initialize-Path $cache "Directory"
-	& dotnet restore $sln --packages $cache
-}
 
 if (-not $SkipClean) {
 	& dotnet nuget locals all --clear
@@ -74,5 +33,3 @@ Build-Cache $Cache $sln
 if (-not $KeepSolution) {
 	Remove-Item "src" -Recurse -Force
 }
-
-#endregion
