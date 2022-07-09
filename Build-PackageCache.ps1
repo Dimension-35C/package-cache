52d483905049264c07c0b7168e4e29de8dcdd965
Jaime Still <jpstill85@gmail.com>
2022-07-08 23:35:07 -0400

    cleanup scripts and update readme

--- Build-PackageCache.ps1
+++ Build-PackageCache.ps1
@@ -1,16 +1,16 @@
 param(
-	[string]
-	[Parameter()]
-	$Cache = "nuget-packages",
-	[string]
-	[Parameter()]
-	$Source = "solution.json",
-	[switch]
-	[Parameter()]
-	$KeepSolution,
-	[switch]
-	[Parameter()]
-	$SkipClean
+    [string]
+    [Parameter()]
+    $Cache = "nuget-packages",
+    [string]
+    [Parameter()]
+    $Source = "solution.json",
+    [switch]
+    [Parameter()]
+    $KeepSolution,
+    [switch]
+    [Parameter()]
+    $SkipClean
 )
 
 . .\functions.ps1
@@ -19,17 +19,17 @@ $data = Get-Content -Raw -Path $Source | ConvertFrom-Json
 
 $sln = "src/BuildCache"
 if (Test-Path $sln) {
-	Remove-Item $sln -Recurse -Force
+    Remove-Item $sln -Recurse -Force
 }
 
 Build-Solution $data $sln
 
 if (-not $SkipClean) {
-	& dotnet nuget locals all --clear
+    & dotnet nuget locals all --clear
 }
 
 Build-Cache $Cache $sln
 
 if (-not $KeepSolution) {
-	Remove-Item "src" -Recurse -Force
+    Remove-Item "src" -Recurse -Force
 }
--- functions.ps1
+++ functions.ps1
@@ -1,12 +1,5 @@
-function Get-ProjectPath(
-    [string] $sln,
-    [string] $project
-) {
-    return Join-Path $sln $project
-}
-
 function Build-Project([psobject] $project, [string] $sln) {
-    $output = Get-ProjectPath $sln $project.name
+    $output = Join-Path $sln $project.name
 
     & dotnet new $project.template -o $output
     & dotnet sln $sln add $output
@@ -24,13 +17,10 @@ function Build-Solution([psobject] $data, [string] $sln) {
     }
 }
 
-function Initialize-Path([string] $path, [string] $type) {
-    if (-not (Test-Path $path)) {
-        New-Item -Path $path -ItemType $type -Force
-    }
-}
-
 function Build-Cache([string] $cache, [string] $sln) {
-    Initialize-Path $cache "Directory"
+    if (-not (Test-Path $cache)) {
+        New-Item -Path $cache -ItemType Directory -Force
+    }
+
     & dotnet restore $sln --packages $cache
 }
